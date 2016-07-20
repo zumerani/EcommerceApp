219ba69f849dbffb5b4332aff983adc235948cbc
Zain Umerani <zumerani@gmail.com>
2016-07-20 00:50:43 -0500

    added configurations file and started up server on localhost

---
+++ Backend/configurations.js
@@ -0,0 +1,7 @@
+exports.database = {
+    url: "mongodb://zumerani:Boeing96@ds023495.mlab.com:23495/schools" , 
+    //collections: ...
+}
+
+exports.port = 8080;
+exports.base = "http://localhost:" + exports.port;
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var server = express();
 
+//Dependencies
 var dependencies = {
     path: require('path') , 
     cors: require('cors') , 
@@ -9,5 +10,28 @@ var dependencies = {
     multer: require('multer') , 
     mongojs: require('mongojs') , 
     morgan: require('morgan') ,
-    //config: require('./config')
+    config: require('./configurations')
 }
+
+//Instantiate our Database (MongoDB)
+var database = dependencies.mongojs( dependencies.config.database.url );
+
+//Set up our backend
+function setUp() {
+    server.use( dependencies.bodyParser.urlencoded( {extended: true } ) );
+    server.use( dependencies.bodyParser.json() );
+    server.use( dependencies.cors );
+}
+
+//Set up data managers (dataManagers)
+
+
+//Set up routes as we go
+
+//Start up our server
+var port = dependencies.config.port;
+server.listen(port);
+
+server.get( '/' , function(req , res) {
+    res.send('Hello, World!');
+});
